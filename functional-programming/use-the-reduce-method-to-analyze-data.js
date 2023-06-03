02b6ff59f26b397a977f39f3616cb9088158b8c7
Shohanul Alam <shohan12224943@gmail.com>
2023-06-03 18:32:25 +0600

    Update and rename use-the-reduce-method-to-analyze-data.txt to use-the-reduce-method-to-analyze-data.js

--- functional-programming/use-the-reduce-method-to-analyze-data.js
+++ functional-programming/use-the-reduce-method-to-analyze-data.js
@@ -113,11 +113,17 @@ const watchList = [
 ];
 
 function getRating(watchList){
-  const averageRating = 
-  watchList.filter(data => data.Director === "Christopher Nolan")
-    .map(data => Number(data.imdbRating))
+  // Add your code below this line
+  const averageRating = watchList
+    // Use filter to find films directed by Christopher Nolan
+    .filter(film => film.Director === "Christopher Nolan")
+    // Use map to convert their ratings from strings to numbers
+    .map(film => Number(film.imdbRating))
+    // Use reduce to add together their ratings
     .reduce((sumOfRatings, rating) => sumOfRatings + rating) /
-  watchList.filter(data => data.Director === "Christopher Nolan").length;
+  // Divide by the number of Nolan films to get the average rating
+  watchList.filter(film => film.Director === "Christopher Nolan").length;
+  // Add your code above this line
   return averageRating;
 }
 
